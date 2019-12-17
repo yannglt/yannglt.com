8f0f6e08cba2034eee3a3140ceb5626c80e2efe3
yannglt <yann-edern.gillet@hetic.net>
2019-12-17 14:05:10 +0100

    [add] mobile + tablet support

---
+++ pages/type.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <div id="type">
+    <h1>Product Designer of things made of strategy, empathy and work precision</h1>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+
+  #type
+    max-width: 1240px
+    margin: 80px auto
+
+</style>
