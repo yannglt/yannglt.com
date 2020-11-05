63bf5a13bbfa2cbfed805da359b8b5ff38be9813
yannglt <helloyannglt@gmail.com>
2020-11-05 11:28:55 +0100

    🔨 Add a dev config for external devices

--- nuxt.config.js
+++ nuxt.config.js
@@ -70,6 +70,14 @@ export default {
   ],
 
   /*
+  ** Run this to be able to inspect on mobile
+  */
+  // server: {
+  //   port: 3000, // default: 3000
+  //   host: '0.0.0.0',
+  // },
+
+  /*
   ** Build configuration
   */
   build: {
