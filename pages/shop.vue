2fcdd4224d86be23444c1e3398b60a604e62c268
yannglt <helloyannglt@gmail.com>
2020-12-25 17:41:05 +0100

    ➕ Add prismjs

---
+++ pages/shop.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div>
+    <h1>Shop</h1>
+  </div>
+</template>
+
+<script>
+  import { NotionRenderer, getPageBlocks } from "vue-notion";
+
+  export default {
+    components: { NotionRenderer },
+    data: () => ({ blockMap: null }),
+    async created() {
+      // get Notion blocks from the API via a Notion pageId
+      this.blockMap = await getPageBlocks("8a783ae641664d0c9841967fe75d3f89");
+    },
+  }
+</script>
