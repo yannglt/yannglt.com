598ecdfcc746a165e0ac5d8ab75d610a29dc1c5b
yannglt <helloyannglt@gmail.com>
2021-02-01 23:13:22 +0100

    🚧 Remove under construction banner emojis

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <a href="#" title="">Welcome on my new space on the Internets.<span class="line-breaker"><br></span><span class="link">Learn more about this new version<svg class="icon" aria-hidden="true" focusable="false"><use xlink:href="icons/sprite.svg#arrow-forward-20"></use></svg></span></a>
     </div> -->
     <div class="under-construction">
-      <a href="#" title="">🚧 New space on the Internets under construction. 🚧<span class="line-breaker"><br></span></a>
+      <a href="#" title="">New space on the Internets under construction.<span class="line-breaker"><br></span></a>
     </div>
     <Navbar></Navbar>
     <section class="tldr">
