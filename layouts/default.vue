127419674527f2fb7a3dc4010316f1c75da135c5
yannglt <helloyannglt@gmail.com>
2021-02-20 22:17:47 +0100

    🐛 Fix new website banner icon

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="new-space">
-      <a href="#" title="">Welcome on my new space on the Internets.<span class="line-breaker"><br></span><span class="link">Learn more about this new version<svg class="icon" aria-hidden="true" focusable="false"><use xlink:href="icons/sprite.svg#arrow-forward-20"></use></svg></span></a>
+      <a href="#" title="">Welcome on my new space on the Internets.<span class="line-breaker"><br></span><span class="link">Learn more about this new version<svg class="icon" aria-hidden="true" focusable="false"><use xlink:href="@/static/icons/sprite.svg#arrow-forward-20"></use></svg></span></a>
     </div>
     <!-- <div class="under-construction">
       <a href="#" title="">New space on the Internets under construction.<span class="line-breaker"><br></span></a>
