0c4071d42af322adfc0ef74d63bc8ee308182e8a
yannglt <helloyannglt@gmail.com>
2021-02-20 22:14:14 +0100

    🚧 Add thanks section

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -107,6 +107,30 @@
       </div>
     </section>
 
+    <section class="thanks">
+      <div class="container">
+        <div class="thanks--website">
+          <div class="tag">This website</div>
+          <div class="content">
+            <p>Born inside Sketch in 2019 and raised inside Figma in 2020/2021. Design tokens pushed by <a href="">Specify</a>.</p>
+            <p>Built on Nuxt and Vue with GreenSock libraries for animations. Hosted by GitHub and distributed by Netflify and Amazon Web Services.</p>
+          </div>
+        </div>
+        <div class="thanks--typefaces">
+          <div class="tag">Typefaces</div>
+          <div class="content">
+            <p>Headlines are set in Bluu Suuperstar, designed by Jean-Baptiste Morizot and redrew in 2017 for french type design company <a href="">Black Foundry</a>.</p>
+            <p>Copy is set in GT America, designed by Noël Leu with Seb McLauchlan for <a href="">Grilli Type</a>.</p>
+          </div>
+        </div>
+        <div class="thanks--people">
+          <div class="tag">A big S/O to this awesome people</div>
+          <div class="content">
+            <p>My partner in life <a href="">Sabrina Nedjah</a> and my brother <a href="">Hassan Boujnkih</a> for their support these last years. <a href="">Gabriel Bacave</a> for its help on writing my manifesto. </p>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
