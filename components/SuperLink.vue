17dbb95f70240928e3851de9eb07c6ee148053c7
yannglt <helloyannglt@gmail.com>
2021-04-04 15:51:12 +0200

    🚧 Add link component

---
+++ components/SuperLink.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <component :is="'a'" :href="href" :class="variant" class="link" >
+    <p v-if="this.$slots.default"><slot/></p>
+    <svg v-if="icon" class="icon" aria-hidden="true" focusable="false">
+      <use :xlink:href="require('@/static/icons/sprite.svg')+icon"></use>
+    </svg>
+  </component>
+</template>
+
+<script>
+  export default {
+    props: {
+      href: { type: String, default: null },
+      variant: { type: String, default: '' },
+      icon: { type: String }
+    }
+  }
+</script>
--- pages/debug.vue
+++ pages/debug.vue
@@ -16,12 +16,23 @@
         <SuperButton href="/notes" variant="tertiary oblivion" icon="#arrow-forward-20">Get Started</SuperButton>
         <SuperButton href="/notes" variant="locked" icon="#forbidden-20">Work under NDA</SuperButton>
       </div>
+      <div class="links dark">
+        <SuperLink href="/notes" variant="emphasis dark" icon="#arrow-forward-20">Read the code behind this website</SuperLink>
+      </div>
+      <div class="links light">
+        <SuperLink href="/notes" variant="emphasis light" icon="#arrow-forward-20">Read the code behind this website</SuperLink>
+        <SuperLink href="/notes" variant="body light">Read the code behind this website</SuperLink>
+      </div>
+      <div class="links info">
+        <SuperLink href="/notes" variant="body info" icon="#arrow-forward-20">Read the code behind this website</SuperLink>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
   import SuperButton from '../components/SuperButton.vue'
+  import SuperLink from '../components/SuperLink.vue'
 
   export default {
     head () {
@@ -38,6 +49,7 @@
 
     components: {
       SuperButton,
+      SuperLink
     }
   }
 </script>
@@ -50,15 +62,18 @@
   .container
     margin-top: 128px
 
-  .buttons
+  .buttons, .links
     grid-column: span 12
     display: flex
     padding: 32px
-    align-items: flex-end
+    align-items: center
 
     &.dark
       background-color: #00030A
 
-    .button:not(:last-of-type)
+    &.info
+      background-color: hsl(216, 100%, 36%)
+
+    .button:not(:last-of-type), .link:not(:last-of-type)
       margin-right: 24px
 </style>
