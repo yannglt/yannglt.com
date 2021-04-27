fe3ec2023213c84be2a6b20ec49818f917d9f566
yannglt <helloyannglt@gmail.com>
2021-04-28 00:14:36 +0200

    💄 Fix copy toast position and responsive

--- components/CopyToast.vue
+++ components/CopyToast.vue
@@ -1,9 +1,9 @@
 <template lang="html">
   <div class="copyToast">
-    <div class="icon--container"><svg class="icon" aria-hidden="true" focusable="false"><use xlink:href="@/static/icons/sprite.svg#check-20"></use></svg></div>
-    <div class="toast--content">
-      <p class="toast--title">Yeah! Email copied in your clipboard</p>
-      <p class="toast--desc">Paste it in your favorite email client, see you soon!</p>
+    <div class="icon-container"><svg class="icon" aria-hidden="true" focusable="false"><use xlink:href="@/static/icons/sprite.svg#check-20"></use></svg></div>
+    <div class="content">
+      <p class="title text-secondary-light">Yeah! Email copied in your clipboard</p>
+      <p class="body desc">Paste it in your favorite email client, <span class="line-breaker"><br></span>see you soon!</p>
     </div>
   </div>
 </template>
