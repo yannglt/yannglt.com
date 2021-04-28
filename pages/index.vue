ce99174acdba90cf8751e32063a3d6c864c0b261
yannglt <helloyannglt@gmail.com>
2021-04-28 11:24:52 +0200

    💄 Fix info banner on > 1440px screens

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template lang="html">
   <div id="app">
-    <div class="new-space container">
-      <a href="/notes/an-introduction-to-my-new-personal-space-on-the-internets" title="" class="body">Welcome on my new space on the Internets.<span class="line-breaker"><br></span><span class="link">Learn more about this new version<svg class="icon" aria-hidden="true" focusable="false"><use xlink:href="@/static/icons/sprite.svg#arrow-forward-20"></use></svg></span></a>
+    <div class="new-space">
+      <div class="container">
+        <a href="/notes/an-introduction-to-my-new-personal-space-on-the-internets" title="" class="body">Welcome on my new space on the Internets.<span class="line-breaker"><br></span><span class="link">Learn more about this new version<svg class="icon" aria-hidden="true" focusable="false"><use xlink:href="@/static/icons/sprite.svg#arrow-forward-20"></use></svg></span></a>
+      </div>
     </div>
 
     <transition name="slide">
